00c2cc6432143706685b391c6e37759e85fd405e
Lucy Qu <lucy_qu@brown.edu>
2021-12-01 18:38:50 -0500

    add file to write to printer

---
+++ pixel-to-printer.py
@@ -0,0 +1,52 @@
+import re 
+import math
+import numpy as np
+import sys
+
+conversion_factor = 50 # mm/pixel
+top_left_x = -6000 # top left coordinate
+top_left_y = -4000
+
+# x, y pixel coordinates to x, y, z cartesian coordinates
+def pixelToXYZ(pixel_point):
+    x = pixel_point[0]
+    y = pixel_point[1]
+
+    x = x * conversion_factor + top_left_x
+    y = y * conversion_factor + top_left_y
+
+    return [x, y, 1000] # fixed z height
+
+P1 = [-170, -99, 150]
+P2 = [0, 196.3, 150]
+P3 = [170, -99, 150]
+home = [0, 0, 150] # in cartesian coordinates
+
+def getHomeInFrameCoordinates():
+    a0 = getEuclideanDistance(home, P1)
+    b0 = getEuclideanDistance(home, P2)
+    c0 = getEuclideanDistance(home, P3)
+    return a0, b0, c0
+    
+def getEuclideanDistance(P1, P2):
+    # convert strings to float
+    for i in range(len(P1)):
+        P1[i] = float(P1[i])
+    for i in range(len(P2)):
+        P2[i] = float(P2[i])
+    return math.sqrt((P2[0]-P1[0])**2 + (P2[1]-P1[1])**2 + (P2[2]-P1[2])**2)
+
+# Cartesian coordinates to lightbot coordinates
+def XYZtoABC(point):
+    a0, b0, c0 = getHomeInFrameCoordinates()
+    A = getEuclideanDistance(P1, point) - a0
+    B = getEuclideanDistance(P2, point) - b0
+    C = getEuclideanDistance(P3, point) - c0
+    return [A, B, C]
+
+pixel_input = input()
+xyz = pixelToXYZ(pixel_input)
+abc = XYZtoABC(xyz)
+
+f = open("/tmp/printer", "w"):
+f.write(abc)
